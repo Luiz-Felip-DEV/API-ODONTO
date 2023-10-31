982098404b60d7a1baf1ad87f822c2cd027e9c81
luiz.souza <luiz.martins@unitri.edu.br>
2023-10-31 16:48:36 -0300

    apagando linha comentada desnecessaria

--- src/routes.js
+++ src/routes.js
@@ -18,24 +18,5 @@ router.put('/password', UserController.putPassword);
 
 //DELETE
 
-// function checkToken(req, res, next) {
-
-//     const authHeader = req.headers['authorization'];
-//     const token      = authHeader && authHeader.split(" ")[1];
-
-//     if (!token) {
-//         return res.status(401).json({msg: "Acesso Negado"});
-//     }
-
-//     try {
-
-//         jwt.verify(token, process.env.SECRET);
-
-//         next()
-
-//     } catch(error) {
-//         res.status(400).json({msg: "Token Invalido!"})
-//     }
-// }
 
 export default router 
